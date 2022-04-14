68d71ccbb9c3ab9f82d4ab738e3f5610ceca0832
Olixfills <oniabrahamfills@gmail.com>
2022-04-14 23:39:50 +0100

    added copy overlay&transition effects

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import CopyToClipboard from 'react-copy-to-clipboard';
 import '../styles/colorBox.css'
 
 
+
 const ColorBox = ({ background }) => {
-    const { color, name } = background;
+const [copied, setCopied] = useState(false)
+const { color, name } = background;
+
+
+  const changeCopyState = () => {
+    setCopied(true)
+    setTimeout(() => {
+      setCopied(false)
+    }, 1500);
+}
+
+
+
   return (
-    <CopyToClipboard text={color} >
-    <div className='color-box' style={{ background: color }}>
+    <CopyToClipboard text={color} onCopy={changeCopyState}>
+      <div className='color-box' style={{ background: color }}>
+        <div className={`copy-overlay ${copied && 'show'}`} style={{ background: color }} />
+        <div className={`copy-msg ${copied && 'show'}`}>
+          <h1>Copied!</h1>
+          <p>{color}</p>
+        </div>
       <div className="copy-container">
         <div className="box-content">
           <span>{name}</span>
